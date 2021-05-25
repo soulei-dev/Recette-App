1dc0244a139a3f7da2370d56cbfb0b8786c81050
Souleimane <soulei.dev@gmail.com>
2021-05-25 10:59:19 +0200

    Added Fonts

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import AppLoading from "expo-app-loading";
+import { useFonts } from "expo-font";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    "Montserrat-Bold": require("./assets/fonts/Montserrat-Bold.ttf"),
+    "Montserrat-Regular": require("./assets/fonts/Montserrat-Regular.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Test default font</Text>
+      <Text style={{ fontFamily: "Montserrat-Bold" }}>
+        Test Montserrat Bold
+      </Text>
+      <Text style={{ fontFamily: "Montserrat-Regular" }}>
+        Test Montserrat Bold
+      </Text>
     </View>
   );
 }
