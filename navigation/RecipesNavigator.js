8ffd2c13c2d299a9624093fadd56c89a48156a95
Souleimane <soulei.dev@gmail.com>
2021-05-25 17:00:26 +0200

    Update navigation to show title in the top tab

--- navigation/RecipesNavigator.js
+++ navigation/RecipesNavigator.js
@@ -1,19 +1,41 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Categories from "../screens/Categories";
 import Filters from "../screens/Filters";
 import Favorites from "../screens/Favorites";
 
 const Tabs = createBottomTabNavigator();
+const CategoriesStack = createStackNavigator();
+const FiltersStack = createStackNavigator();
+const FavoritesStack = createStackNavigator();
+
+const CategoriesStackScreen = () => (
+  <CategoriesStack.Navigator>
+    <CategoriesStack.Screen name="Home" component={Categories} />
+  </CategoriesStack.Navigator>
+);
+
+const FiltersStackScreen = () => (
+  <FiltersStack.Navigator>
+    <FiltersStack.Screen name="Filtrer" component={Filters} />
+  </FiltersStack.Navigator>
+);
+
+const FavoritesStackScreen = () => (
+  <FavoritesStack.Navigator>
+    <FavoritesStack.Screen name="Favoris" component={Favorites} />
+  </FavoritesStack.Navigator>
+);
 
 export default function RecipesNavigator() {
   return (
     <NavigationContainer>
       <Tabs.Navigator>
-        <Tabs.Screen name="Home" component={Categories}></Tabs.Screen>
-        <Tabs.Screen name="Filtrer" component={Filters}></Tabs.Screen>
-        <Tabs.Screen name="Favoris" component={Favorites}></Tabs.Screen>
+        <Tabs.Screen name="Home" component={CategoriesStackScreen} />
+        <Tabs.Screen name="Filtrer" component={FiltersStackScreen} />
+        <Tabs.Screen name="Favoris" component={FavoritesStackScreen} />
       </Tabs.Navigator>
     </NavigationContainer>
   );
